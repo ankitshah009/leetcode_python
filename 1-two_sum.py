6ce8aca6b51b778e11e6d40a3cd9b51e96e84b1c
Ankit Shah <ankit.tronix@gmail.com>
2020-10-30 23:33:04 -0400

    updated_java_solution

--- 1-two_sum.py
+++ 1-two_sum.py
@@ -33,3 +33,15 @@ class Solution(object):
             if target-num in dict1:
                 return[dict1[target-num],i]
             dict1[num] = i 
+
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
