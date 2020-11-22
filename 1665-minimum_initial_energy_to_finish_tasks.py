8f47eadf7be875c77ebf15fd2003ddc3c78fde3b
Ankit Shah <ankit.tronix@gmail.com>
2020-11-22 13:40:43 -0500

    update

--- 1665-minimum_initial_energy_to_finish_tasks.py
+++ 1665-minimum_initial_energy_to_finish_tasks.py
@@ -135,3 +135,17 @@ class Solution:
             ans = min(cur-minimum, ans)
             cur -= cost
         return -ans
+
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        tasks.sort(key=lambda x: [x[0]-x[1],-x[1],x[0]])
+        energy=0
+        energyTotal=0
+        for a,b in tasks:
+            e=energy-b
+            if e<0:
+                energy-=e
+                energyTotal-=e
+            energy-=a
+            
+        return energyTotal
